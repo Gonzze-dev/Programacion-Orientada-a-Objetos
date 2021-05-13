60ef8ce0bd783f71119a5a1f6079c35919491530
errandoneagonzalo18@gmail.com <Gonzalo>
2021-05-12 22:12:52 -0300

    arregle el tp 4

--- TP 4/MapaMundi.java
+++ TP 4/MapaMundi.java
@@ -1,87 +1,15 @@
 package paquete;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.Scanner;
 
 public class MapaMundi 
 {
-	public static Continente buscar_Continente(String nombre_Continente, MapaMundial obj_Mapa_Mundial)
-	{
-		ArrayList<Continente> mapa_Mundial = new ArrayList<Continente>();
-		mapa_Mundial = obj_Mapa_Mundial.get_Mapa_Mundial();
-		
-		for (int i = 0; i < mapa_Mundial.size(); i++)
-		{
-			if	(mapa_Mundial.get(i).get_Nombre_Continente().equals(nombre_Continente))
-			{
-					return mapa_Mundial.get(i);
-			}
-			
-		}
-		
-		return null;
-	}
-	
-	public static String listar_Paises_Del_Continente(Continente obj_Continente)
-	{
-		String listado_Paises = "";
-		
-		for (int i = 0; i < obj_Continente.get_Paises().size(); i++) 
-		{
-			listado_Paises += obj_Continente.get_Pais(i).get_Nombre_Pais() + "\n";
-		}
-		
-		return listado_Paises;
-	}
-	
-	public static Pais buscar_Pais(String nombre_Pais, MapaMundial obj_Mapa_Mundial)
-	{
-		ArrayList<Continente> mapa_Mundial = new ArrayList<Continente>();
-		mapa_Mundial = obj_Mapa_Mundial.get_Mapa_Mundial();
-		
-		for (int i = 0; i < mapa_Mundial.size(); i++)
-		{
-			
-			for (int j = 0; j < mapa_Mundial.size(); j++) 
-			{
-				
-				if	(mapa_Mundial.get(i).get_Pais(j).get_Nombre_Pais().equals(nombre_Pais))
-				{
-					return mapa_Mundial.get(i).get_Pais(j);
-				}
-				
-			}
-			
-		}
-		
-		return null;
-		
-	}
-	
-	public static String listar_Provincias_Del_Pais(Pais obj_Pais)
-	{
-		String listado_Provincias = "";
-		
-		for (int i = 0; i < obj_Pais.get_Provincias().size(); i++) 
-		{
-			
-			listado_Provincias += obj_Pais.get_Provincia(i).get_Nombre_Provincia() + "\n";
-			
-		}
-		
-		return listado_Provincias;
-	}
-	
+
 	public static void Menu(MapaMundial obj_Mapa_Mundial, Scanner escaner)
 	{
 		int opcion = 0;
-		String nombre = "";
-		
-		Continente obj_continente = new Continente();
-		Pais obj_Pais = new Pais();
+		String nombre = "", listado_Paises = "", listado_Provincias = "";
 		
 		System.out.print("Ingrese elija una opcion: ");
 		opcion = Integer.parseInt(escaner.nextLine()); 
@@ -92,11 +20,12 @@ public class MapaMundi
 				
 				System.out.print("\nIngrese el nombre del continente: ");
 				nombre = escaner.nextLine();
-				obj_continente = buscar_Continente(nombre, obj_Mapa_Mundial);
 				
-				if (obj_continente != null)
+				listado_Paises = obj_Mapa_Mundial.get_Paises(nombre);
+				
+				if (listado_Paises != null)
 				{
-					System.out.print(listar_Paises_Del_Continente(obj_continente));
+					System.out.print(listado_Paises);
 				}
 				else
 				{
@@ -108,11 +37,12 @@ public class MapaMundi
 				
 				System.out.print("\nIngrese el nombre del Pais: ");
 				nombre = escaner.nextLine();
-				obj_Pais = buscar_Pais(nombre, obj_Mapa_Mundial);
 				
-				if (obj_Pais != null)
+				listado_Provincias = obj_Mapa_Mundial.get_Provincias(nombre);
+				
+				if (listado_Provincias != null)
 				{
-					System.out.print(listar_Provincias_Del_Pais(obj_Pais));
+					System.out.print(listado_Provincias);
 				}
 				else
 				{
--- TP 4/MapaMundial.java
+++ TP 4/MapaMundial.java
@@ -44,21 +44,114 @@ public class MapaMundial
 		
 	}
 	
-	public MapaMundial(ArrayList<Continente> mapa_Mundial) 
-	{
-		super();
-		this.mapa_Mundial = mapa_Mundial;
-	}
-	
 	public ArrayList<Continente> get_Mapa_Mundial() 
 	{
 		return this.mapa_Mundial;
 	}
-
-	public void set_Mapa_Mundial(ArrayList<Continente> mapa_Mundial)
+	
+	private Continente buscar_Continente(String nombre_Continente, ArrayList<Continente> mapa_Mundial)
 	{
-		this.mapa_Mundial = mapa_Mundial;
+		
+		for (int i = 0; i < mapa_Mundial.size(); i++)
+		{
+			if	(mapa_Mundial.get(i).get_Nombre_Continente().equals(nombre_Continente))
+			{
+					return mapa_Mundial.get(i);
+			}
+			
+		}
+		
+		return null;
 	}
 	
+	private String listar_Paises_Del_Continente(Continente obj_Continente)
+	{
+		String listado_Paises = "";
+		
+		for (int i = 0; i < obj_Continente.get_Paises().size(); i++) 
+		{
+			listado_Paises += obj_Continente.get_Pais(i).get_Nombre_Pais() + "\n";
+		}
+		
+		return listado_Paises;
+	}
+	
+	public String get_Paises(String nombre_Continente)
+	{
+		Continente obj_continente = new Continente();
+		String listado_Paises;
+		obj_continente = buscar_Continente(nombre_Continente, this.mapa_Mundial);
+		
+		if(obj_continente != null)
+		{
+			listado_Paises = listar_Paises_Del_Continente(obj_continente);
+		}
+		else
+		{
+			listado_Paises = null;
+		}
+		
+		obj_continente = null;
+		
+		return listado_Paises;
+	}
+	
+	public Pais buscar_Pais(String nombre_Pais, ArrayList<Continente> Mapa_Mundial)
+	{
+		
+		for (int i = 0; i < mapa_Mundial.size(); i++)
+		{
+			
+			for (int j = 0; j < mapa_Mundial.get(i).get_Paises().size(); j++) 
+			{
 
+				if	(mapa_Mundial.get(i).get_Pais(j).get_Nombre_Pais().equals(nombre_Pais))
+				{
+					
+					return mapa_Mundial.get(i).get_Pais(j);
+					
+				}
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public String listar_Provincias_Del_Pais(Pais obj_Pais)
+	{
+		String listado_Provincias = "";
+
+		for (int i = 0; i < obj_Pais.get_Provincias().size(); i++) 
+		{
+			
+			listado_Provincias += obj_Pais.get_Provincia(i).get_Nombre_Provincia() + "\n";
+			
+		}
+
+		return listado_Provincias;
+	}
+	
+	public String get_Provincias(String nombre_Provincia)
+	{
+		Pais obj_Pais = new Pais();
+		String listado_Provincias;
+		
+		obj_Pais = buscar_Pais(nombre_Provincia, this.mapa_Mundial);
+		
+		if(obj_Pais != null)
+		{
+			listado_Provincias = listar_Provincias_Del_Pais(obj_Pais);
+		}
+		else
+		{
+			listado_Provincias = null;
+		}
+		
+		obj_Pais = null;
+		
+		return listado_Provincias;
+	}
 }
